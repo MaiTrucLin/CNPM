2665a160f76c2e2d7a94a2f392b69ee3d56a7605
Phạm Trung Dũng <trungdungit45@gmail.com>
2016-10-30 16:37:06 +0700

    hello

--- XDPMQL_CuahangPKGaming/Interface/Form_QLKDPK.Designer.cs
+++ XDPMQL_CuahangPKGaming/Interface/Form_QLKDPK.Designer.cs
@@ -28,11 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.danhMụcSảnPhẩmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.báoCáoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.danhSáchHóaĐơnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dáToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.danhSáchHóaĐơnToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.danhMụcSảnPhẩmToolStripMenuItem,
+            this.báoCáoToolStripMenuItem,
+            this.danhSáchHóaĐơnToolStripMenuItem,
+            this.dáToolStripMenuItem,
+            this.danhSáchHóaĐơnToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1003, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // danhMụcSảnPhẩmToolStripMenuItem
+            // 
+            this.danhMụcSảnPhẩmToolStripMenuItem.Name = "danhMụcSảnPhẩmToolStripMenuItem";
+            this.danhMụcSảnPhẩmToolStripMenuItem.Size = new System.Drawing.Size(129, 20);
+            this.danhMụcSảnPhẩmToolStripMenuItem.Text = "Danh mục sản phẩm";
+            // 
+            // báoCáoToolStripMenuItem
+            // 
+            this.báoCáoToolStripMenuItem.Name = "báoCáoToolStripMenuItem";
+            this.báoCáoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.báoCáoToolStripMenuItem.Text = "Báo cáo";
+            // 
+            // danhSáchHóaĐơnToolStripMenuItem
+            // 
+            this.danhSáchHóaĐơnToolStripMenuItem.Name = "danhSáchHóaĐơnToolStripMenuItem";
+            this.danhSáchHóaĐơnToolStripMenuItem.Size = new System.Drawing.Size(121, 20);
+            this.danhSáchHóaĐơnToolStripMenuItem.Text = "Danh sách hóa đơn";
+            // 
+            // dáToolStripMenuItem
+            // 
+            this.dáToolStripMenuItem.Name = "dáToolStripMenuItem";
+            this.dáToolStripMenuItem.Size = new System.Drawing.Size(32, 20);
+            this.dáToolStripMenuItem.Text = "dá";
+            // 
+            // danhSáchHóaĐơnToolStripMenuItem1
+            // 
+            this.danhSáchHóaĐơnToolStripMenuItem1.Name = "danhSáchHóaĐơnToolStripMenuItem1";
+            this.danhSáchHóaĐơnToolStripMenuItem1.Size = new System.Drawing.Size(121, 20);
+            this.danhSáchHóaĐơnToolStripMenuItem1.Text = "Danh sách hóa đơn";
+            // 
+            // Form_QLKDPK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form_QLKDPK";
+            this.ClientSize = new System.Drawing.Size(1003, 481);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form_QLKDPK";
+            this.Text = "Quản lý cửa hàng kinh doanh phụ kiện Gaming Gear";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem danhMụcSảnPhẩmToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem báoCáoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem danhSáchHóaĐơnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dáToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem danhSáchHóaĐơnToolStripMenuItem1;
     }
 }
